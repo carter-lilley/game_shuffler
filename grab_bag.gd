e2252479ff4f0a5bff4deacaf91b7a86ec3a5955
carter-lilley <56591072+carter-lilley@users.noreply.github.com>
2024-07-19 16:45:19 -0400

    First Commit - Basic Random & Grab Bag Modes

---
+++ grab_bag.gd
@@ -0,0 +1,191 @@
+extends Button
+
+@onready var sound_player = $"../../../AudioStreamPlayer2D"
+@onready var user_settings = $"../../../user_settings"
+
+var system_list: PackedStringArray
+var cmd_arr: Array
+var prc_list: Array 
+func _ready() -> void:
+	fill_prc_list(user_settings.bag_size)
+	print(prc_list)
+
+	system_list = exclude_sys(dir_contents(user_settings.rom_dir), ["gc", "n3ds","ps2","ps3","psvita","psx","switch","wii","wiiu", "xbox", "xbox360"])
+	for i in range(user_settings.bag_size):
+		var curr_sys: String = rand_string(system_list)
+		var curr_sys_dir: String = user_settings.rom_dir + "\\" + curr_sys
+		
+		var curr_core = match_core(curr_sys)
+		
+		var games_arr: PackedStringArray = DirAccess.get_files_at(curr_sys_dir)
+		var curr_game: String = rand_string(games_arr)
+		
+		var args: PackedStringArray = ["-L" , user_settings.ra_cores_dir + curr_core , user_settings.rom_dir + "\\" + curr_sys + "\\" + curr_game]
+		cmd_arr.append(args)
+	print(cmd_arr)
+	
+func _on_pressed() -> void:
+	nextGame(-1)
+	pass # Replace with function body.
+
+func nextGame(last_pid: int):
+	sound_player.play()
+	var pssuspend_path = "C:\\Users\\carter\\Documents\\game_launcher\\PSTools\\pssuspend.exe"
+	var curr_id = randi() % user_settings.bag_size
+	print("Current_ID: ", curr_id, " Last_PID: ", last_pid)
+	for prc_info in prc_list:
+	#suspend all active processes..
+		if prc_info["pid"] == last_pid:
+			prc_info["active"] = false
+			var suspend = PackedStringArray([last_pid])
+			var result = OS.execute(pssuspend_path,suspend, [], true)
+			if result != OK:
+				print("Failed to suspend process: ", prc_info["pid"])
+			else:
+				print("Suspending Process: ", prc_info["pid"])
+	#find the current process. if this process is new, create & assign it - else, resume it
+		if prc_info["id"] == curr_id:
+			if prc_info["pid"] == 0:
+				prc_info["pid"] = OS.create_process(user_settings.ra, cmd_arr[curr_id])
+				if prc_info["pid"] != 0:
+					prc_info["active"] = true
+					print("Creating Process: ", prc_info["pid"])
+				else:
+					print("Failed to create new process.")
+			else:
+				if !prc_info["active"]:
+					prc_info["active"] = true
+					var resume = PackedStringArray(["-r", prc_info["pid"]])
+					var result = OS.execute(pssuspend_path,resume, [], true)
+					if result != OK:
+						print("Failed to resume process: ", prc_info["pid"])
+					else:
+						print("Resuming Process: ", prc_info["pid"])
+	#print(prc_list)
+	last_pid = prc_list[curr_id]["pid"]
+	bringWindowToFront(prc_list[curr_id]["pid"])
+	create_timer(randf_range(user_settings.round_time_min, user_settings.round_time_max),nextGame, last_pid)
+
+#------- FUNCTIONS
+func fill_prc_list(x: int):
+	for i in range(x):
+		var prc_info = {
+			"id": i,
+			"pid": 0,
+			"active": false  # or false, depending on your initial state
+		}
+		prc_list.append(prc_info)
+
+func bringWindowToFront(pid: int):
+	var bringgtofront_path = "C:\\Users\\carter\\Documents\\game_launcher\\PSTools\\bringtofront.ps1"
+	var args = [
+		"-Command",
+		"Set-ExecutionPolicy Bypass -Scope Process;",
+		". " + bringgtofront_path + "; goto " + str(pid)
+	]
+	var output = []
+	var exit_code = OS.execute("powershell.exe", args, output, true)
+	if exit_code == 0:
+		print("PowerShell script executed successfully.")
+		print("Output:")
+		print(output)  # Output array contains the entire shell output as a single String element
+	else:
+		print("Error executing PowerShell script.")
+		print("Error Output:")
+		print(output)  # Output array contains the error message if execution fails
+
+func match_core(sys : String) -> String:
+	var current_core: String
+	match sys:
+		"3do":
+			current_core = user_settings.core_3do
+		"atari2600":
+			current_core = user_settings.core_atari2600
+		"atarilynx":
+			current_core = user_settings.core_atarilynx
+		"dos":
+			current_core = user_settings.core_dos
+		"dreamcast":
+			current_core = user_settings.core_dreamcast
+		"gamegear":
+			current_core = user_settings.core_gamegear
+		"gb":
+			current_core = user_settings.core_gb
+		"gba":
+			current_core = user_settings.core_gba
+		"gbc":
+			current_core = user_settings.core_gbc
+		"genesis":
+			current_core = user_settings.core_genesis
+		"mastersystem":
+			current_core = user_settings.core_mastersystem
+		"n64":
+			current_core = user_settings.core_n64
+		"nds":
+			current_core = user_settings.core_nds
+		"neogeo":
+			current_core = user_settings.core_neogeo
+		"nes":
+			current_core = user_settings.core_nes
+		"ngpc":
+			current_core = user_settings.core_ngpc
+		"psp":
+			current_core = user_settings.core_psp
+		"saturn":
+			current_core = user_settings.core_saturn
+		"sega32x":
+			current_core = user_settings.core_sega32x
+		"segacd":
+			current_core = user_settings.core_segacd
+		"sg1000":
+			current_core = user_settings.core_sg1000
+		"snes":
+			current_core = user_settings.core_snes
+		"tg16":
+			current_core = user_settings.core_tg16
+		"tgcd":
+			current_core = user_settings.core_tgcd
+	return current_core
+
+func exclude_sys(dir_arr: PackedStringArray, exclusion_arr: Array) -> PackedStringArray:
+	print("Searching for exclusions..")
+	var trimmed_arr: PackedStringArray = []
+	for i in range(dir_arr.size()):
+		var dir_name = dir_arr[i]
+		if !exclusion_arr.has(dir_name):
+			trimmed_arr.push_back(dir_name)
+		else:
+			print("Excluded: ", dir_name)
+	return trimmed_arr
+
+func rand_string(dir_arr: PackedStringArray) -> String:
+	var rand_string: String = dir_arr[randi() % dir_arr.size()]
+	return rand_string
+
+func create_timer(secs: float, function: Callable, arg: Variant):
+	var timer = Timer.new()
+	timer.wait_time = secs
+	timer.one_shot = true
+	timer.autostart = true
+	add_child(timer)  # Add the timer as a child of the current node (assuming this is a Node or Control)
+	# Connect the timeout signal to call the specified function
+	timer.timeout.connect(function.bind(arg))
+	return timer
+
+func dir_contents(path) -> PackedStringArray:
+	var dir = DirAccess.open(path)
+	var dir_arr : PackedStringArray
+	if dir:
+		dir.list_dir_begin()
+		var entry = dir.get_next()
+		while entry != "":
+			if dir.current_is_dir():
+				dir_arr.append(entry)
+				print("Found dir: " + entry)
+			else:
+				print("Found file: " + entry)
+			entry = dir.get_next()
+		return dir_arr
+	else:
+		print("An error occurred when trying to access the path.")
+		return dir_arr
---
+++ user_settings.gd
@@ -0,0 +1,48 @@
+extends Node
+
+var bag_size: int = 4
+var round_time_min : float = 15.0
+var round_time_max : float = 180.0
+#-------------------------------
+var rom_dir: String = "Z:\\roms"
+#--------- RA
+var ra_dir: String = "D:\\Emulation\\storage\\retroarch"
+var ra: String = ra_dir + "\\retroarch.exe"
+var ra_cores_dir: String = ra_dir + "\\cores"
+#--------- CORES
+var core_3do: String = "\\opera_libretro.dll"
+var core_atari2600: String = "\\stella_libretro.dll"
+var core_atarilynx: String = "\\handy_libretro.dll"
+var core_dos: String = "\\dosbox_pure_libretro.dll"
+var core_dreamcast: String = "\\flycast_libretro.dll"
+var core_gamegear: String = "\\genesis_plus_gx_libretro.dll"
+var core_gb: String = "\\gambatte_libretro.dll"
+var core_gba: String = "\\mgba_libretro.dll"
+var core_gbc: String = "\\gambatte_libretro.dll"
+#var core_gc: String = "\\mesen_libretro"
+var core_genesis: String = "\\genesis_plus_gx_libretro.dll"
+var core_mastersystem: String = "\\genesis_plus_gx_libretro.dll"
+#var core_n3ds: String = "\\mesen_libretro"
+var core_n64: String = "\\mupen64plus_next_libretro.dll"
+var core_nds: String = "\\melonds_libretro.dll"
+var core_neogeo: String = "\\fbalpha2012_neogeo_libretro.dll"
+var core_nes: String = "\\mesen_libretro.dll"
+var core_ngpc: String = "\\mednafen_ngp_libretro.dll"
+#var core_ps2: String = "\\mesen_libretro"
+#var core_ps3: String = "\\mesen_libretro"
+var core_psp: String = "\\ppsspp_libretro.dll"
+#var core_psvita
+#var core_psx: String = "\\mesen_libretro"
+var core_saturn: String = "\\mednafen_saturn_libretro.dll"
+var core_sega32x: String = "\\picodrive_libretro.dll"
+var core_segacd: String = "\\genesis_plus_gx_libretro.dll"
+var core_sg1000: String = "\\genesis_plus_gx_libretro.dll" #? special character
+var core_snes: String = "\\snes9x_libretro.dll"
+#var core_switch: String = "\\mesen_libretro"
+var core_tg16: String = "\\mednafen_pce_libretro.dll"
+var core_tgcd: String = "\\mednafen_pce_libretro.dll" #? special character
+#var core_wii: String = "\\mesen_libretro"
+#var core_wiiu
+#var core_xbox
+#var core_xbox360
+#---------
